d2ffaab149b0c7c329ba4a879a8a274750c8873a
Otniel Kevin Abiel <okabiel0112@pnm.co.id>
2024-08-26 21:44:46 +0700

    fix: fix vendor access

--- routes/vendor.routes.js
+++ routes/vendor.routes.js
@@ -9,7 +9,7 @@ const isAuthenticate = require("../middlewares/authentication");
 router.get("/", vendor.getAllVendor);
 router.get("/:id_vendor", vendor.getOneVendor);
 
-// router.use(isAuthenticate);
+router.use(isAuthenticate);
 router.post("/", validate(createVendor), vendor.createVendor);
 router.patch("/:id_vendor", vendor.updateVendor);
 router.delete("/:id_vendor", vendor.deleteVendor);
