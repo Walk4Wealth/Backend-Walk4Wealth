778f0740b43c2ca693805c26a4c433e28254a9cc
Otniel Kevin Abiel <okabiel0112@pnm.co.id>
2024-08-31 18:24:20 +0700

    fix validation auth

--- services/auth/validators/auth.validator.js
+++ services/auth/validators/auth.validator.js
@@ -1,15 +1,6 @@
 const { check } = require("express-validator");
 
 const registerValidator = [
-  check("nama")
-    .exists()
-    .withMessage("Must have nama")
-    .bail()
-    .notEmpty()
-    .withMessage("Can not be empty")
-    .bail()
-    .isString()
-    .withMessage("Must be string"),
   check("username")
     .exists()
     .withMessage("Must have username")
